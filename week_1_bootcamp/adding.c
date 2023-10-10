014fb1925410e65d63c194f27d1a78fed30a2b68
AristaaSingh <146640656+AristaaSingh@users.noreply.github.com>
2023-10-10 14:01:32 +0000

    c bootcamp 1

---
+++ week_1_bootcamp/adding.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+    float a = 10.34;
+    float b = 20.34;
+    float c = a+b;
+    printf("The sum of ading %f and %f is %d\n");
+}
---
+++ week_1_bootcamp/first.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+    printf(" My name is Aristaa. You can call me Ari. ");
+    return 0;
+}
